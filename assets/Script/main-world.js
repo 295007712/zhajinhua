fd09b446fe30a951254a39f74e72087309cc72c3
蔡遵造 <caizunzao@4399inc.com>
2018-11-21 14:43:08 +0800

    socket ok

--- assets/Script/main-world.js
+++ assets/Script/main-world.js
@@ -12,23 +12,15 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        edit_box: {
+            default: null,
+            type: cc.EditBox
+        }
     },
-
+    buttonClick: function (event, cunstomData){
+        console.log('1111' + cunstomData);
+        console.log('edit_box content: ' + this.edit_box.string);
+    },
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},

20184e16c97cad19fffad871341dc2077321a2ab
蔡遵造 <caizunzao@4399inc.com>
2018-11-22 09:50:05 +0800

    1

--- assets/Script/main-world.js
+++ assets/Script/main-world.js
@@ -11,7 +11,9 @@ cc.Class({
     buttonClick: function (event, cunstomData){
         console.log('1111' + cunstomData);
         console.log('edit_box content: ' + this.edit_box.string);
-        global.eventlistener.fire("login", this.edit_box.string);
+        if(this.edit_box.string !== 0){
+            global.eventlistener.fire("login", this.edit_box.string);
+        }
     },
     // LIFE-CYCLE CALLBACKS:
 
